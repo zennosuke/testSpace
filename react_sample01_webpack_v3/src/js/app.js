a2b372dfdc53118979cdf6c3542131d0b771b313
zennosuke <zen.e.0719@gmail.com>
2019-08-06 08:46:19 +0900

    app.jsを追記

--- react_sample01_webpack_v3/src/js/app.js
+++ react_sample01_webpack_v3/src/js/app.js
@@ -5,3 +5,23 @@ ReactDOM.render(
   <h1>Hello, world!</h1>,
   document.getElementById('app1')
 );
+
+function formatName(user) {
+  return user.firstName + user.lastName;
+}
+
+var user = {
+  firstName: 'zen',
+  lastName: 'nosuke'
+};
+
+var element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('app2')
+);
